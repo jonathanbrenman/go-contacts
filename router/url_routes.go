ff98b225f1a8e8e875ed86ac9626d020ef357e0a
Jonathan Brenman <jonathan.brenman@mercadolibre.com>
2020-08-15 20:07:17 -0300

    adding search support

--- router/url_routes.go
+++ router/url_routes.go
@@ -9,9 +9,10 @@ func (r routerCtx) routes() {
 	
 	// Define Routes Here
 	r.router.GET("/ping", pingController.Ping)
-	r.router.GET("/contacts/:id", contactController.GetContact)
+	r.router.GET("/contacts/search", contactController.Search)
+	r.router.GET("/contacts/id/:id", contactController.GetContact)
 	r.router.GET("/contacts", contactController.GetAll)
 	r.router.POST("/contacts/update", contactController.Update)
 	r.router.POST("/contacts", contactController.Add)
-	r.router.DELETE("/contacts/:id", contactController.Delete)
+	r.router.DELETE("/contacts/id/:id", contactController.Delete)
 }
